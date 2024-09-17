ecc74f77538344261e1fc2ca671817049722fa83
fergsosa <134444526+fergsosa@users.noreply.github.com>
2024-09-17 16:45:22 -0300

    test

--- js/cart.js
+++ js/cart.js
@@ -62,7 +62,7 @@ const cart = () => {
           <div class="image">
             <img src="/images/${info.id}.png">
           </div>
-          <div class="name">${info.name}</div>
+          <div class="name">${info.imgDetail}</div>
           <div class="totalPrice">$${info.price * item.quantity}</div>
           <div class="quantity">
             <span class="minus" data-id="${info.id}"> < </span>
