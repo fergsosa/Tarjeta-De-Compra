46e67198ae10fe9cb36da5305c3fcbab985bfe3a
fergsosa <134444526+fergsosa@users.noreply.github.com>
2024-09-17 18:18:17 -0300

    test

--- js/detail.js
+++ js/detail.js
@@ -28,7 +28,7 @@ const initApp = () => {
   }
 
   let detail = document.querySelector(".detail");
-  detail.querySelector(".image img").src = "thisProduct imgDetail";
+  detail.querySelector(".image img").src = "../" + thisProduct.imgDetail;
   detail.querySelector(".name").innerText = thisProduct.name;
   detail.querySelector(".price").innerText = "$" + thisProduct.price;
   detail.querySelector(".description").innerText =
